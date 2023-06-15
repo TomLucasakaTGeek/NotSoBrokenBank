37319ce96a9ccd3563d4e4ad537cebf22137dbd3
TomLucasakaTGeek <120933239+TomLucasakaTGeek@users.noreply.github.com>
2023-06-15 15:07:14 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,59 @@
+import java.util.*;
+class Main {
+  public static void main(String args[]){
+    int balance = 10000,withdraw,deposit;
+    Scanner o = new Scanner(System.in);
+
+    while(true)
+      {
+       System.out.println("Automated Teller Machine");
+       System.out.println("Enter 1 to Withdraw");
+       System.out.println("Enter 2 to Deposit");
+       System.out.println("Enter 3 to Check Balance");
+       System.out.println("Enter 4 to EXIT");
+       System.out.println("Enter your Choice of Operation");
+
+        int choice = o.nextInt();
+        switch(choice)
+          {
+            case 1:
+             
+              System.out.print("Enter money to be withdrawn:");
+              withdraw = o.nextInt();
+
+              if(balance >= withdraw)
+              {
+                 balance -= withdraw;
+                 System.out.println("Le Pakad");
+              }
+              else{
+                System.out.println("Insufficient balance");
+              }
+              System.out.println("");
+              
+               break;
+                
+            case 2:
+              
+              System.out.print("Enter money to be deposited:");
+                deposit = o.nextInt();
+
+              balance += deposit;
+              System.out.println("Le Daal Diya");
+              System.out.println("");
+                
+               break;
+
+            case 3:
+              
+              System.out.println("Balance: "+balance);
+              System.out.println("");
+
+               break;
+
+            default:
+               System.exit(0);
+          }
+        }
+      }
+    }  
